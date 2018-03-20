75ad0fc8e0ce96893628a8ce7dd65a3075d304db
Sonu <sonukn089@gmail.com>
2018-03-20 22:12:33 +0530

    first php page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP PAGE</title>
+</head>
+<body>
+
+	<h1>My first page!</h1>
+
+	<?php
+
+		echo "Hello Sonu !";
+		echo "<br>";
+		echo "Hello Monu !";
+		echo "<br>";
+		echo "Hello Pushpender !";
+		echo "<br>";
+		echo "Hello Neeraj !";
+	?>
+
+</body>
+</html>
+
+
