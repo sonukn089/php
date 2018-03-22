cc0869dd9d0161c751888deb0b0deba1d13e8c93
Sonu <sonukn089@gmail.com>
2018-03-23 00:02:54 +0530

    add something

--- index.php
+++ index.php
@@ -64,6 +64,20 @@
 		$y = 4;
 		echo $x + $y . " !";
 	?>
+	<hr>
+	<?php
+		$x = 5; // global scope
+ 
+		function myTest() {
+    		// using x inside this function will generate an error
+    		echo "<p>Variable x inside function is: $x</p>";
+		} 
+		myTest();
+
+		echo "<p>Variable x outside function is: $x</p>";
+	?>
+	
+
 </body>
 </html>
 
