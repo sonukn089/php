9323fd3f37a79fd593128f64e1310040a9cddf49
Sonu <sonukn089@gmail.com>
2018-03-23 23:14:02 +0530

    echo and print

---
+++ echoandprint.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>echo and print Statements</title>
+</head>
+<body>
+
+	<?php
+
+		echo "<h2>PHP is Fun!</h2>";
+		echo "Hello world!<br>";
+		echo "I'm about to learn PHP!<br>";
+		echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+	?>
+
+
+</body>
+</html>
