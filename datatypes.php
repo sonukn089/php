8373ed040465cf237e739328cfc31df1e6dd3bcb
Sonu <sonukn089@gmail.com>
2018-03-24 23:20:29 +0530

    data types

---
+++ datatypes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Types</title>
+</head>
+<body>
+	<?php 
+		$x = "Hello world!";
+		$y = 'Hello world!';
+
+		echo $x;
+		echo "<br>"; 
+		echo $y;
+	?>
+
+</body>
+</html>
