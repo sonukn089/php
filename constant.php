00be144208bfd21d7043b6ac3467f0d577c54d66
Sonu <sonukn089@gmail.com>
2018-03-27 00:42:40 +0530

    constant

---
+++ constant.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Constant</title>
+</head>
+<body>
+
+	<?php
+
+		define("GREETING", "Welcome to W3school.com");
+
+		echo GREETING;
+
+	?>
+
+</body>
+</html>
