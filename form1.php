384a14b77c24bd278cf2aa578d705dc0ed1fe521
Sonu <sonukn089@gmail.com>
2018-03-30 23:41:39 +0530

    form1

--- form1.php
+++ form1.php
@@ -5,5 +5,22 @@
 </head>
 <body>
 
+	<form method="post" action="processform.php">
+		<table>
+			<tr>
+				<td>Name :</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>Email :</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="submit" value="send"></td>
+			</tr>
+		</table>		
+	</form>
+
 </body>
 </html>
