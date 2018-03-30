24000100df5e4c356a5b81064365eb6aedc843b8
Sonu <sonukn089@gmail.com>
2018-03-30 23:38:45 +0530

    add tr & td

--- form.php
+++ form.php
@@ -6,16 +6,37 @@
 <body>
 
 	<form method="post" action="process.php">
-
-		<div> Name : <input type="text" name="name" placeholder="name"></div><br/>
-		<div> Email : <input type="text" name="email" placeholder="email address"></div><br/>
-		<div> Address : <input type="text" name="address"></div><br/>
-		<div> City : <input type="text" name="city" placeholder="city"></div><br/>
-		<div> Passord : <input type="password" name="password"></div><br/>
-		<div> Confirmation Passord : <input type="password" name="confirmpassword"></div><br/>
-
-		<input type="submit" name="submit" value="submit">
-		
+		<table>	
+			<tr>
+				<td>Name :</td>
+				<td><input type="text" name="name" placeholder="name"></td>
+			</tr>
+			<tr>
+				<td>Email :</td>
+				<td><input type="text" name="email" placeholder="email address"></td>
+			</tr>
+			
+			<tr>
+				<td>Address :</td>
+				<td><input type="text" name="address"></td>
+			</tr>
+			<tr>
+				<td>City :</td>
+				<td><input type="text" name="city" placeholder="city"></td>
+			</tr>
+			<tr>
+				<td>Password :</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td>Confirmation Passord :</td>
+				<td><input type="password" name="confirmpassword"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="submit" value="submit"></td>
+			</tr>	
+		</table>
 
 	</form>
 
