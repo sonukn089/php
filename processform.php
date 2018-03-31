78b9105c5866b1095b5ca2684df489a51780148c
Sonu <sonukn089@gmail.com>
2018-03-31 23:53:05 +0530

    processform.php

--- processform.php
+++ processform.php
@@ -1,6 +1,13 @@
 <?php
 
-	echo "Name : ". $_POST['name']. "<br>";
-	echo "Email : ". $_POST['email']. "<br>";
+	// echo "Name : ". $_POST['name']. "<br>";
+	// echo "Email : ". $_POST['email']. "<br>";
+
+	// echo "Name : ". $_GET['name']. "<br>";
+	// echo "Email : ". $_GET['email']. "<br>";
+
+
+	echo "Name : ". $_REQUEST['name']. "<br>";
+	echo "Email : ". $_REQUEST['email']. "<br>";
 
 ?>
