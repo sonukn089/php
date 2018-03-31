53fabc60424c78c2b058d4b082b8053603abf168
Sonu <sonukn089@gmail.com>
2018-04-01 00:13:59 +0530

    insert data in sql

---
+++ formdatabase.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Base</title>
+</head>
+<body>
+
+	<table>
+		<tr>
+			<td>Name :</td>
+			<td><input type="text" name="name"></td>
+		</tr>
+		<tr>
+			<td>Email :</td>
+			<td><input type="text" name="email"></td>
+		</tr>
+		<tr>
+			<td>Password :</td>
+			<td><input type="password" name="password"></td>
+		</tr>
+	</table>
+
+</body>
+</html>
