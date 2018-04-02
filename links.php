16bdce58e502b1bd04e429fe1694c69e305a06f0
Sonu <sonukn089@gmail.com>
2018-04-03 00:23:51 +0530

    updated all fiel

--- links.php
+++ links.php
@@ -1,7 +1,7 @@
 
 <a href="home.php">Home</a>&nbsp;&nbsp;
 <a href="formdatabase.php">Register</a>&nbsp;&nbsp;
-<a href="insert.php">Edit User</a>&nbsp;&nbsp;
+<a href="update.php">Edit User</a>&nbsp;&nbsp;
 <a href="output.php">See Data</a>&nbsp;&nbsp;
 <a href="delete.php">Delete User</a>
 
