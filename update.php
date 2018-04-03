9256adf1ebd4b7fbeb642a9f3afd7b85422b3793
Sonu <sonukn089@gmail.com>
2018-04-03 23:24:52 +0530

    edit file

--- update.php
+++ update.php
@@ -8,6 +8,26 @@
 
 	echo "<table width=\"90%\" align=center border=2>";
 	echo "<tr><td width=\"40%\" align=center bgcolor=\"FFFF00\">ID</td>
+		 <td width==\"40%\" align=center bgcolor=\"FFFF00\">NAME</td>
+		 <td width==\"40%\" align=center bgcolor=\"FFFF00\">EMAIL</td>
+		 <td width==\"40%\" align=center bgcolor=\"FFFF00\">PASSWORD</td></tr>";
+
+
+	while ($row=mysql_fetch_array($result)) {
+		
+		 $id=$row['id'];
+		 $name=$row['name'];
+		 $email=$email['email'];
+		 $password=$password['password'];
+
+		 echo "<tr><td align=center>
+		 	  <a href=\edit.php?id=$id&name=$name&email=$email&password=$password\">$id</a></td>
+		 	  <td>$name</td><td>$email</td><td>$password</td></tr>";	
+	}	 
+
+	echo "</table>";
+
+	mysql_close();
 
 
 
