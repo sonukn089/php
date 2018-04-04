96f586c2d287484c56bd09d711c5e6bb7b207a12
Sonu <sonukn089@gmail.com>
2018-04-05 00:26:34 +0530

    created updateform.php file

--- links.php
+++ links.php
@@ -1,11 +1,13 @@
 
-<a href="home.php">Home</a>&nbsp;&nbsp;
-<a href="formdatabase.php">Register</a>&nbsp;&nbsp;
-<a href="update.php">Edit User</a>&nbsp;&nbsp;
-<a href="output.php">See Data</a>&nbsp;&nbsp;
-<a href="delete.php">Delete User</a>
 
-
+<center>
+	<h3>	
+		<a href="home.php">Home</a>&nbsp;&nbsp;
+		<a href="formdatabase.php">Register</a>&nbsp;&nbsp;
+		<a href="update.php">Edit User</a>&nbsp;&nbsp;
+		<a href="delete.php">Delete User</a>
+	</h3>
+</center>
 
 
 	
