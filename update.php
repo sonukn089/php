feee08d797ec9f3a6c8d9eb2a6a69ce655d2eddb
Sonu <sonukn089@gmail.com>
2018-04-04 22:43:13 +0530

    all file updated

--- update.php
+++ update.php
@@ -4,21 +4,21 @@
 
 	mysql_select_db("testsite");
 
-	$result = mysql_query(SELECT * FROM users);
+	$result = mysql_query("SELECT * FROM users");
 
-	echo "<table width=\"90%\" align=center border=2>";
-	echo "<tr><td width=\"40%\" align=center bgcolor=\"FFFF00\">ID</td>
-		 <td width==\"40%\" align=center bgcolor=\"FFFF00\">NAME</td>
-		 <td width==\"40%\" align=center bgcolor=\"FFFF00\">EMAIL</td>
-		 <td width==\"40%\" align=center bgcolor=\"FFFF00\">PASSWORD</td></tr>";
-
+	echo "<table width=\"75%\" align=center border=2>";
+	
+	echo "<tr><td width=\"20%\" align=center bgcolor=\"FFFF00\">ID</td>
+		 <td width==\"30%\" align=center bgcolor=\"FFFF00\">NAME</td>
+		 <td width==\"30%\" align=center bgcolor=\"FFFF00\">EMAIL</td>
+		 <td width==\"30%\" align=center bgcolor=\"FFFF00\">PASSWORD</td></tr>";
 
 	while ($row=mysql_fetch_array($result)) {
 		
 		 $id=$row['id'];
 		 $name=$row['name'];
-		 $email=$email['email'];
-		 $password=$password['password'];
+		 $email=$row['email'];
+		 $password=$row['password'];
 
 		 echo "<tr><td align=center>
 		 	  <a href=\edit.php?id=$id&name=$name&email=$email&password=$password\">$id</a></td>
