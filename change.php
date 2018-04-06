83830e3c9df2f62b3eea370aec87259262555e32
Sonu <sonukn089@gmail.com>
2018-04-07 00:33:47 +0530

    some edit in change.php file

--- change.php
+++ change.php
@@ -3,7 +3,13 @@
 	$id = $_REQUEST['id'];
 	$newname = $_REQUEST['newname'];
 	$newemail = $_REQUEST['newemail'];
-	
+	$newpassword = $_REQUEST['newpassword'];
+
+	mysql_connect("localhost","root","") or die ("Problems with connection!");
+
+	mysql_select_db("testsite");
+
+	mysql_query("UPDATE users SET name='$newname',email='$newemail', password='$newpassword' WHERE id='$id'");
 
 
 ?>
