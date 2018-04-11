e832e92f19aab599e59111c1f1e51c34c5bf6ebd
Sonu <sonukn089@gmail.com>
2018-04-12 01:04:35 +0530

    add some file

---
+++ process11.php
@@ -0,0 +1,9 @@
+<?php
+
+	echo $_POST['name']. "<br/>";
+	echo $_POST['email']. "<br/>";
+	echo $_POST['adress']. "<br/>";
+	echo $_POST['password']. "<br/>";
+	echo $_POST['confirmpassword'];
+
+?>
