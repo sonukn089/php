3978405054431f3566a270d8954e5cdfc3349292
Sonu <sonukn089@gmail.com>
2018-04-13 00:24:56 +0530

    MultidimensionalArray.php

---
+++ MultidimensionalArray.php
@@ -0,0 +1,15 @@
+<?php
+
+	$users = array("Victor" =>"Sonu", "Bill" =>"Pushpendra", "Monu" =>"Neeraj Singh", "AA"=>"S");
+
+	echo $users["Victor"];
+	echo "<br>";
+	echo $users["Bill"];
+	echo "<br>";
+	echo $users["Monu"];
+	echo "<br>";
+	echo $users["AA"];
+
+
+
+?>
