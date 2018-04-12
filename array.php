683d8d2d2ca945e8b58c26f3a20e6d51b1bc78dd
Sonu <sonukn089@gmail.com>
2018-04-12 23:53:39 +0530

    create array many types

--- array.php
+++ array.php
@@ -1,16 +1,29 @@
 <?php
 
-	// $str = "Hello Sonu";
-	// echo $str;
+	$str = "Hello Sonu";
+	echo $str;
 
-	// $var = 123;
-	// echo $var;
+	echo "<br>";
+
+	$var = 123;
+	echo $var;
+	echo "<br>";
+
+	$numbers = array(10, 25, 33, 46, 53);
+	echo $numbers[1];
 	
-	// $numbers = array(1, 2, 3, 4, 5);
-	// echo $numbers[0];
-	
+	echo "<br>";
+
 	$numbers = array("Victor", "Alex", "Matt", "Bill");
 	echo $numbers[3]." " .$numbers[1];
 
+	echo "<br>";
+
+	$numbers[0] = "Sonu";
+	$numbers[1] = "Pushpendra";
+	$numbers[2] = "Neeraj";
+	$numbers[3] = "Monu";
+
+	echo $numbers[2];
 
 ?>
