56e4dff3af3067a773ec3a5e4edcdb9ba15381f6
Sonu <sonukn089@gmail.com>
2018-04-13 01:04:22 +0530

    create Multi array

--- MultidimensionalArray.php
+++ MultidimensionalArray.php
@@ -1,15 +1,18 @@
 <?php
 
-	$users = array("Victor" =>"Sonu", "Bill" =>"Pushpendra", "Monu" =>"Neeraj Singh", "AA"=>"S");
+	// $users = array("Victor" =>"Sonu", "Bill" =>"Pushpendra", "Monu" =>"Neeraj Singh", "AA"=>"S");
 
-	echo $users["Victor"];
-	echo "<br>";
-	echo $users["Bill"];
-	echo "<br>";
-	echo $users["Monu"];
-	echo "<br>";
-	echo $users["AA"];
+	// echo $users["Victor"];
+	// echo "<br>";
+	// echo $users["Bill"];
+	// echo "<br>";
+	// echo $users["Monu"];
+	// echo "<br>";
+	// echo $users["AA"];
+	//               index-0     index-1         index-1 ka 0
+	$students = array("Victor", array("30", array("boy","girl", array("boy","tt") )));
 
 
+	echo $students[1][1][2][0]; 
 
 ?>
