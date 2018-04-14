b1370a4ff06d32807e956fb92f9d3fe9e2d0f0a1
Sonu <sonukn089@gmail.com>
2018-04-15 01:07:19 +0530

    create switch statment

---
+++ switch.php
@@ -0,0 +1,42 @@
+<?php
+
+	$i = 2;
+
+	switch ($i) {
+
+		case 0:
+			echo "the value is 0"; break;
+
+		case 1:
+			echo "the value is 1"; break;
+			
+		case 2:
+			echo "the value is 2"; break;
+			
+		default:
+			echo "Not match your case "; break;
+			
+	}
+	echo "<br>";
+	echo "<hr>";
+
+	$i = "Sonu";
+
+	switch ($i) {
+
+		case "Sonu":
+			echo "Welcome Sonu"; break;
+
+		case "Monu":
+			echo "the value is 1"; break;
+			
+		case "Pushpendra":
+			echo "the value is 2"; break;
+			
+		default:
+			echo "Not match your case "; break;
+			
+	}
+
+
+?>
