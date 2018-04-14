3024b2995c29753a4fd57b9f3a077a62be0603d0
Sonu <sonukn089@gmail.com>
2018-04-15 01:15:11 +0530

    create include file

---
+++ include.php
@@ -0,0 +1,13 @@
+<?php
+	
+	include("home.php");
+	echo "<hr>";
+	include ("logical.php");
+	echo "<hr>";
+	include ("loop.php");
+	echo "<hr>";
+	include ("array.php");
+	echo "<hr>";
+	include ("MultidimensionalArray.php");
+
+?>
